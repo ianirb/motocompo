d100802b0fcfe98e73d243ace2fc74767c7050fb
ianirb <theianirby@gmail.com>
2025-02-07 14:49:15 -0600

    Updated Layout & Cards

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { DivideIcon as LucideIcon } from 'lucide-react';
+import { LucideIcon } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import { Button } from './Button';
 
@@ -73,8 +73,7 @@ export function ServiceCard({ title, description, icon: Icon, color }: ServiceCa
             style={{
               backgroundColor: 'black',
               color: 'white',
-              boxShadow: `0 0 20px ${color}20`,
-              '--tw-ring-color': color
+              boxShadow: `0 0 20px ${color}20`
             }}
           >
             Learn More
