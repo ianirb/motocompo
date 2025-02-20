86b2aac566fed6dca43be4ee990e4c49473d340d
ianirb <theianirby@gmail.com>
2025-02-19 18:46:14 -0600

    MAJOR - SUPABASE DATA UPDATE

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { MessageSquare, Database, Users, Bot, Phone, Calendar, Quote, BarChart, Clock } from 'lucide-react';
 import { Button } from '../components/Button';
 
